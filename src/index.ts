e57e83e389869aa5d417ae3866810be5d285917f
Syeda Zainab <syedazainab400@gmail.com>
2026-08-25 20:03:34 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ interface Item {
 }
 
 export function createItem(id: string, name: string, price: number): Item {
-  return { id, name, price };
+  return { id, name, price, trackingId: "ABC-123" };
 }
 
 console.log(createItem("1", "Widget", 9.99));
